5f69acdd720d940c569dd6af934cb1b6601a1d48
Kassian Houben <kassian@mineiros.io>
2022-02-02 21:31:31 +1300

    feat: add initial implementation

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,16 @@
 # OUTPUT ALL RESOURCES AS FULL OBJECTS
 # ----------------------------------------------------------------------------------------------------------------------
 
+output "topic" {
+  value       = try(google_pubsub_topic.topic[0], {})
+  description = "The created pub sub resource."
+}
+
+output "iam" {
+  description = "The iam resource objects that define access to the GCS bucket."
+  value       = { for key, iam in module.topic-iam : key => iam.iam }
+}
+
 # ----------------------------------------------------------------------------------------------------------------------
 # OUTPUT MODULE CONFIGURATION
 # ----------------------------------------------------------------------------------------------------------------------
@@ -14,8 +24,3 @@ output "module_enabled" {
   description = "Whether or not the module is enabled."
   value       = var.module_enabled
 }
-
-# output "module_tags" {
-#   description = "A map of tags that will be applied to all created resources that accept tags."
-#   value       = var.module_tags
-# }
